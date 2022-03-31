ee2dbb35b47b2c608bf7d8d750f24089d3ec75ad
Ravi Kumar Ranga <ravimcaanurag@gmail.com>
2022-03-31 21:56:49 +0530

    Test

---
+++ DevLab002/Controllers/FisrtController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevLab002.Controllers
+{
+    public class FisrtController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
